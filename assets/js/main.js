07a5f869cf6176fdbf45d7f14fe61783b9c8cdb6
Jozz16 <jo633554@gmail.com>
2024-04-03 13:53:55 -0300

    Se agrego tooltip en nav

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// tooltip
+$(function () {
+    $('#inicio').tooltip({ title: "Inicio", placement: "bottom" });
+    $('#quienesSomos').tooltip({ title: "Quienes Somos", placement: "bottom" });
+    $('#destacados').tooltip({ title: "Destacados", placement: "bottom" });
+    $('#contacto').tooltip({ title: "Contacto", placement: "bottom" });
+});
+
