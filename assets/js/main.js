16f95650da1c8b29275c7afc57991f850c2c5c8b
Jozz16 <jo633554@gmail.com>
2024-04-03 15:22:42 -0300

    fix:scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,19 @@
 $(function () {
     $('#inicio').tooltip({ title: "Inicio", placement: "bottom" });
     $('#quienesSomos').tooltip({ title: "Quienes Somos", placement: "bottom" });
-    $('#destacados').tooltip({ title: "Destacados", placement: "bottom" });
-    $('#contacto').tooltip({ title: "Contacto", placement: "bottom" });
+    $('#destacadosNav').tooltip({ title: "Destacados", placement: "bottom" });
+    $('#contactoNav').tooltip({ title: "Contacto", placement: "bottom" });
 });
 
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            var scrollPosition = target.offset().top - 50; // Ajuste aquí el valor (-50 es solo un ejemplo)
+            $('html, body').stop().animate({
+                scrollTop: scrollPosition
+            }, 1000);
+        }
+    });
+});
